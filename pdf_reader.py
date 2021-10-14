3784766a236ac32b70302dfeb5777b69a05014ca
sagar kumar <sagarkumarsameer@gmail.com>
2021-10-14 23:49:03 +0530

    pdf book reader

---
+++ pdf_reader.py
@@ -0,0 +1,16 @@
+import pyttsx3
+from pyttsx3 import engine
+import PyPDF2
+def speak(text):
+    engine= pyttsx3.init()
+    voices=engine.getProperty("voices")
+    engine.setProperty('voice',voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+book_name=open('pythonstandard.pdf','rb')
+pdfReader=PyPDF2.PdfFileReader(book_name)
+pages=pdfReader.numPages
+print(pages)
+page=pdfReader.getPage(1)
+book=page.extractText()
+speak(book)
